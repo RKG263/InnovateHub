fed7230c64e9b5fdbd66e273e9ff0690d204bc2b
Rahul <thisisrkg@gmail.com>
2024-05-22 12:36:45 +0530

    login change

--- client/src/Components/Auth/Login.jsx
+++ client/src/Components/Auth/Login.jsx
@@ -62,12 +62,18 @@ const Login = () => {
             name="confirmPassword"
             required
           />
-          <input
-            type="text"
-            placeholder="Role"
-            name="role"
+           <div>
+            <select
+              name="role"
+              required
 
-          />
+            >
+              <option value="">Select Role</option>
+              <option value="Mentor">Mentor</option>
+              <option value="Investor">Investor</option>
+              <option value="Entreprenaur">Entreprenaur</option>
+            </select>
+          </div>
           <div
             style={{
               gap: "10px",
