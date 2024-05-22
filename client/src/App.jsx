daafc50518815d41066325f135d4e69b39fd581e
smartysagar21 <smartysagar21@gmail.com>
2024-05-22 13:39:53 +0530

    update user profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-
-import React from "react"
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Components/Auth/Login";
 import About from "./Pages/About";
@@ -10,77 +9,53 @@ import Register from "./Components/Auth/Register";
 import Home from "./Components/Home/Home";
 import NotFound from "./Shared/NotFound";
 import SpinningLoader from "./Shared/SpinningLoader";
-
-
+import EnterpreneurProfile from "./Pages/Users/Firm/EnterpreneurProfile";
 
 function App() {
-  
   const isAuthenticated = false;
   return (
-     
-     <BrowserRouter>
-       
-       <Routes>
-        <Route path="/" element ={<Home/>}/>
-       <Route 
-              path="/login"
-              element={
-                <ProtectedRoute
-                  user={!isAuthenticated}
-                  redirect="/"
-                >
-                  <Login/>
-               </ProtectedRoute>
-              }
-            />
-       <Route 
-              path="/register"
-              element={
-                <ProtectedRoute
-                  user={!isAuthenticated}
-                  redirect="/"
-                >
-                  <Register/>
-               </ProtectedRoute>
-              }
-            />
-          <Route
-              path="/about"
-              element={
-                <About/>
-              }
-            />
-             <Route
-              path="/contact"
-              element={
-                <ProtectedRoute
-                  user={isAuthenticated}
-                  redirect="/login"
-                >
-                  <Contact/>
-               </ProtectedRoute>
-              }
-            />
-             <Route
-              path="/profile"
-              element={
-                <ProtectedRoute
-                  user={isAuthenticated}
-                  redirect="/login"
-                >
-                  <Profile/>
-               </ProtectedRoute>
-              }
-            />
-           <Route path="/spinload" element={<SpinningLoader/>} />
-             <Route path="*" element={<NotFound/>} />
-
-
-
-       </Routes>
-     </BrowserRouter>
-      
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={
+            <ProtectedRoute user={!isAuthenticated} redirect="/">
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <ProtectedRoute user={!isAuthenticated} redirect="/">
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/contact"
+          element={
+            <ProtectedRoute user={isAuthenticated} redirect="/login">
+              <Contact />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute user={isAuthenticated} redirect="/login">
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/spinload" element={<SpinningLoader />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/user/profile" element={<EnterpreneurProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- client/src/Pages/Users/Firm/EnterpreneurProfile.jsx
+++ client/src/Pages/Users/Firm/EnterpreneurProfile.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./enterpreneurProfile.css";
 
 const EnterpreneurProfile = () => {
   return (
-    <div>EnterpreneurProfile</div>
-  )
-}
+    <div className="profile">
+      <div className="picture">
+        <img
+          src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <div className="rating">STAR</div>
+      </div>
+      <div className="detail">
+        <div className="nameDetail">
+          <div className="name">LANA DEL RE</div>
+          <div className="user"> Entrepreneur</div>
+        </div>
+        <div className="bio">
+          Please help. I am full of talents. But due to my familly background I
+          am unable to grow in my career.
+        </div>
+        <div className="moreDetail">
+          <button>Update profile</button>
+          <button>My Mentor</button>
+          <button>My investor</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EnterpreneurProfile
+export default EnterpreneurProfile;
