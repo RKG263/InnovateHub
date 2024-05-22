73879b75bb2bd6f4141ea710525006b93a41dc04
Prashant Kumar <1prashantkumar1@gmail.com>
2024-05-22 19:56:58 +0530

    models added

--- models/mentorModel.js
+++ models/mentorModel.js
@@ -1,1 +1,40 @@
 import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+
+const mentorSchema = new Schema({
+    experties: {
+        type: String,
+
+    },
+    post: {
+        type: String,
+
+    },
+    chats: {
+        type: String,
+
+    },
+    rating: {
+        type: String,
+
+    },
+    contact: {
+        type: String,
+
+    },
+    askForMentorship: {
+        type: String,
+
+    },
+    mentorshipGiven: {
+        type: String,
+
+    }
+
+
+
+});
+
+
+export default mongoose.model("mentor", mentorSchema);
