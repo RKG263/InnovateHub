9a0e3171f14aa22e1e7a933966a17333f7ed4571
Rahul <thisisrkg@gmail.com>
2024-05-22 10:09:47 +0530

    blog

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,17 +1,9 @@
 import express from 'express'
 import { isauth } from '../middleware/authMiddleware.js';
-<<<<<<< HEAD
 import { createPostController } from '../controllers/blogController.js';
 
 const router=express.Router();
 
 router.post('/post',isauth,createPostController);
-=======
-import { postController } from '../controllers/blogController.js';
-
-const router=express.Router();
-
-router.post('/post', isauth, postController);
->>>>>>> 9af59804d2c702850f0a9baa9281d511e8a87e3d
 
 export default router
