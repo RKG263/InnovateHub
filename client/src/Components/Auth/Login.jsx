78ca6f99425b4336cc70320cf8953e4b4211f44b
Rahul <thisisrkg@gmail.com>
2024-05-23 05:52:28 +0530

    login modified

--- client/src/Components/Auth/Login.jsx
+++ client/src/Components/Auth/Login.jsx
@@ -6,6 +6,7 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { useDispatch } from 'react-redux';
 import { login } from "../../redux/actions/user";
+import { Link } from "react-router-dom";
 
 
 
@@ -51,12 +52,18 @@ const Login = () => {
             name="confirmPassword"
             required
           />
-          <input
-            type="text"
-            placeholder="Role"
-            name="role"
-            onChange={e => setRole(e.target.value)}
-          />
+          <div>
+            <select
+              name="role"
+              required
+              onChange={e => setRole(e.target.value)}
+            >
+              <option value="">Select Role</option>
+              <option value="Mentor">Mentor</option>
+              <option value="Investor">Investor</option>
+              <option value="Entreprenaur">Entreprenaur</option>
+            </select>
+          </div>
           <div
             style={{
               gap: "10px",
