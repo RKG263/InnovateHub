ef2db1df2fd57fbeb67a96bd925cee8eb3f36fe2
Prashant Kumar <1prashantkumar1@gmail.com>
2024-05-23 06:20:41 +0530

    added controllers

--- client/src/redux/actions/user.js
+++ client/src/redux/actions/user.js
@@ -6,11 +6,6 @@ export const login = (email, password , role) => async dispatch => {
     dispatch({ type: 'loginRequest' });
 
     const { data } = await axios.post(
-<<<<<<< HEAD
-      `${import.meta.env.VITE_API_ENDPOINT}/api/v1/auth/login`,
-      { email, password , role},
-      {
-=======
       `${server}/auth/login`,
       { email, password , role},
       {
@@ -18,7 +13,6 @@ export const login = (email, password , role) => async dispatch => {
         //   'Content-type': 'application/json',
         // },
 
->>>>>>> 34959a4bb0adb4f315ea83832c7c77d5beb276fc
         withCredentials: true,
       }
     );
@@ -28,17 +22,6 @@ export const login = (email, password , role) => async dispatch => {
     dispatch({ type: 'loginFail', payload: error.response.data.message });
   }
 };
-<<<<<<< HEAD
-
-
-
-export const register = formdata => async dispatch => {
-    try {
-      dispatch({ type: 'registerRequest' });
-  
-      const { data } = await axios.post(`${import.meta.env.VITE_API_ENDPOINT}/api/v1/auth/register`, formdata, {
-    
-=======
 export const register = (name , email , password , role)  => async dispatch => {
     try {
       dispatch({ type: 'registerRequest' }); 
@@ -47,7 +30,6 @@ export const register = (name , email , password , role)  => async dispatch => {
         //   'Content-type': 'multipart/form-data',
         // },
   
->>>>>>> 34959a4bb0adb4f315ea83832c7c77d5beb276fc
         withCredentials: true,
       });
   
@@ -60,11 +42,7 @@ export const register = (name , email , password , role)  => async dispatch => {
     try {
       dispatch({ type: 'logoutRequest' });
   
-<<<<<<< HEAD
-      const { data } = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/api/v1/auth/logout`, {
-=======
       const { data } = await axios.get(`${server}/auth/logout`, {
->>>>>>> 34959a4bb0adb4f315ea83832c7c77d5beb276fc
             withCredentials: true,
       });
       dispatch({ type: 'logoutSuccess', payload: data.message });
