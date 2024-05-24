4a952ac5c3a4ef47d847235aeeea62ebd7215cb7
Prashant Kumar <1prashantkumar1@gmail.com>
2024-05-24 23:45:22 +0530

    improved css of dashboard

--- client/src/Components/Users/Enterpreneur/Idea.jsx
+++ client/src/Components/Users/Enterpreneur/Idea.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 const Idea = () => {
 
-  const [ideas, setIdeas] = useState([1,2,3]);
+  const [ideas, setIdeas] = useState([1,2,3,5,5,5,5]);
 
 
     useEffect(()=>{
@@ -26,32 +26,32 @@ const Idea = () => {
   )
 }
 
-export default Idea
+export default Idea;
 
 
 const genrateIdea = (idea)=>{
 
-
-    
-
     return (
       <>
-      <div className='flex flex-col mb-4 border border-black'>
+      <div className='flex flex-col mb-4 rounded-lg shadow-lg  bg-gradient-to-r from-blue-500 to-green-500 p-4 text-white max-w-[35vw]'>
 
-        <div className='flex  '>
+        <div className='flex m-4 justify-evenly'>
 
-          <div className='flex flex-col w-[50%]'>
-            <div>Title</div>
-            <div>Idea</div>
+          <div className='flex flex-col w-[50%] '>
+            <h1 className='mb-2 font-extrabold text-3xl text-center'> Title</h1>
+            <div className='text-justify p-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perspiciatis eveniet quae asperiores, sint in doloremque perferendis praesentium unde, quis a temporibus quos earum? Et aperiam sequi accusamus necessitatibus harum.</div>
           </div>
 
-          <div className='flex flex-col w-[48%]'>
+          <div className='flex flex-col w-[48%]  p-2'>
 
-            <div className='flex justify-center items-center'>
-              <Avatar/>
+            <div className='flex justify-center items-center mb-8'>
+              <Avatar
+                sx={{ width: 96, height: 96 }}
+                className='border-[3px] border-white '
+              />
             </div>
 
-            <div className='flex flex-col'>
+            <div className='flex flex-col text-right'>
               <div>Investor Name</div>
               <div>Email</div>
               <div>Contact</div>
@@ -59,7 +59,7 @@ const genrateIdea = (idea)=>{
           </div>
         </div>
 
-          <div>
+          <div className='mb-2 pl-2 font-semibold'>
             Status : Pending / Success
           </div>
       </div>
--- client/src/Pages/Users/Firm/EnterpreneurProfile.jsx
+++ client/src/Pages/Users/Firm/EnterpreneurProfile.jsx
@@ -1,61 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import "./enterpreneurProfile.css";
+import Post from "../../../Components/Users/Post.jsx";
+import Chat from "../../../Components/Users/Chat.jsx";
+import EditProfile from "../../../Components/Users/EditProfile.jsx";
+import Meeting from "../../../Components/Users/Meeting.jsx";
+import Idea from "../../../Components/Users/Enterpreneur/Idea.jsx";
+import Investors from "../../../Components/Users/Enterpreneur/Investors.jsx";
+import Mentors from "../../../Components/Users/Enterpreneur/Mentors.jsx";
+
 
 const EnterpreneurProfile = () => {
+
+  
+  const [currentComponent, setCurrentComponent] = useState("Post");
+
+
+  const handleClick = (event) => {
+
+    setCurrentComponent(event.target.getAttribute('name'));
+
+  }
+
+  const genreateComponent = () => {
+    switch (currentComponent) {
+      case 'Post':
+        return <Post />
+      case 'Chat':
+        return <Chat />
+      case 'EditProfile':
+        return <EditProfile />
+      case 'Investors':
+        return <Investors />
+      case 'Idea':
+        ; return <Idea />
+      case 'Mentors':
+        return <Mentors />
+      case 'Meeting':
+        return <Meeting />
+      default:
+        return <Post />
+    }
+
+
+  }
+
+
   return (
     
       
-      <div className="container">
+      <div className="container justify-evenly">
         {/* <!-- Profile Section --> */}
 
-        <div className="profile-section">
-          <div className="flex justify-center items-center flex-col text-center">
-            
-            <img
-              src="https://images.pexels.com/photos/3781545/pexels-photo-3781545.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt=""
-              className="profile-img"
-              />
-            <h2 className="user-name">URFI JAVED</h2>
 
-            <p className="user-bio">
-              Innovating solutions to create a better tomorrow.
-              <br />
-              Contact - 9087890878
-            </p>
+          <div className="profile-section">
+            <div className="flex justify-center items-center flex-col text-center ">
+              
+              <img
+                src="https://images.pexels.com/photos/3781545/pexels-photo-3781545.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+                className="profile-img"
+                />
+              <h2 className="user-name">URFI JAVED</h2>
+
+              <p className="user-bio">
+                Innovating solutions to create a better tomorrow.
+                <br />
+                Contact - 9087890878
+              </p>
+                </div>
+            <div className="profile-content">
+
+              <div className="link"
+                onClick={handleClick}
+              >
+                <div className="link-info"
+                  name="Post"
+                >Post</div>
+                <div className="link-info"
+                  name="Idea"
+                >Ideas Submitted</div>
+                <div className="link-info"
+                  name="Meeting"
+                >Meeting Schedule</div>
+                <div className="link-info"
+                  name="Mentors"
+                >Mentors</div>
+                <div className="link-info"
+                  name="Investors"
+                >Investors</div>
+                <div className="link-info"
+                  name="Chat"
+                >Chat</div>
+                <div className="link-info"
+                  name="EditProfile"
+                >Edit Profile</div>
               </div>
-          <div className="profile-content">
-
-            <div className="link"
-              onClick={handleClick}
-            >
-              <div className="link-info"
-                name="Post"
-              >Post</div>
-              <div className="link-info"
-                name="Idea"
-              >Ideas Submitted</div>
-              <div className="link-info"
-                name="Meeting"
-              >Meeting Schedule</div>
-              <div className="link-info"
-                name="Mentors"
-              >Mentors</div>
-              <div className="link-info"
-                name="Investors"
-              >Investors</div>
-              <div className="link-info"
-                name="Chat"
-              >Chat</div>
-              <div className="link-info"
-                name="EditProfile"
-              >Edit Profile</div>
             </div>
           </div>
-        </div>
+        
+
+
+
+
+
         {/* <!-- Post Section --> */}
-        <div className="post-section">{genreateComponent()}</div>
-      </div>
+        <div className="post-sections  p-2  min-w-[45%] overflow-auto max-h-[100vh]">{genreateComponent()}</div>
+      </div>                                              
       
   );
 };
